73e12ec7bab5da71dcb397a2a67175e128f914b2
Mobina Mosannafat <mobinamosannafat@Mobinas-MacBook-Air.local>
2023-12-15 18:02:37 -0700

    storing metrics data for 500 sample added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from collections import deque
 import numpy as np
 import random
 import itertools
+import pickle
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -14,12 +15,12 @@ warnings.filterwarnings('ignore')
 GAMMA = 0.9                   #Care more about recent steps
 BATCH_SIZE = 32
 BUFFER_SIZE = 1000
-MIN_REPLAY_SIZE = 32          #To Do #Parham: 1000 => Care less about previous ones
-MAX_STEPS = 2000
+MIN_REPLAY_SIZE = 32          #To Do #1000 => Care less about previous ones
+MAX_STEPS = 2000000
 TARGET_UPDATE_FREQ = 128
-HIDDEN_LAYERS_SIZE = 64       #To Do
+HIDDEN_LAYERS_SIZE = 64       #To Do 32 for crelu
 LEARNING_RATE = 0.01
-#SWAP_EVERY = 500
+SWAP_EVERY = 10000
 SEED = 6555 #should be loop
 AVERAGE_WINDOW_SIZE = 1000
 
@@ -40,44 +41,59 @@ def set_random_seed():
 
 
 class DQN(nn.Module):
-    def __init__(self, env, hidden_size=128):
+    def __init__(self, env, hidden_size=128,name=''):
         super(DQN, self).__init__()
         in_features = int(np.product(env.observation_spec().shape))
         n_actions = env.action_spec().num_values
 
         self.fc1 = nn.Linear(in_features=in_features, out_features=hidden_size).to(device)
-        # self.relu = nn.ReLU()
         self.fc2 = nn.Linear(in_features=hidden_size, out_features=n_actions).to(device)  # multiply by 2 because of the CRelu
 
+        self.plate = None
+
+        self.name=name
+
+        self.fc1_input=None
+        self.fc1_output=None
+        self.act_output=None
+        self.output_output=None
 
         self.to(device)
-        self.activation_l0norm = []
-        self.activation_l1norm = []
-        self.activation_l2norm = []
+    
 
     def crelu(self, x):
         # Apply ReLU separately to positive and negative parts
         return torch.cat((torch.relu(x), torch.relu(-x)), dim=1)  # Concatenate along dim=1
 
     def forward(self, x):
+
+        self.fc1_input=x
+
         output = self.fc1(x)
 
+        self.fc1_output=output
+        
+
         # Relu
-        # output = nn.ReLU()(output)
+        output = nn.ReLU()(output)
+
+        self.act_output=output
+        
 
         # Leaky Relu
-        output = nn.LeakyReLU(0.1)(output)
+        # output = nn.LeakyReLU(0.1)(output)
 
         # CRelu
         # output = self.crelu(output)
 
-        l0_norm = torch.norm(output, p=0, dim=1).mean().item()  # L0 norm
-        l1_norm = torch.norm(output, p=1, dim=1).mean().item()  # L1 norm
-        l2_norm = torch.norm(output, p=2, dim=1).mean().item()  # L2 norm
-        self.activation_l0norm.append(l0_norm)
-        self.activation_l1norm.append(l0_norm)
-        self.activation_l2norm.append(l0_norm)
+        self.plate = output
+
+
+
         output = self.fc2(output)
+
+        self.output_output=output
+
         return output
 
     def act(self, obs):
@@ -95,12 +111,12 @@ def sample_action(env):
     actions = np.arange(env.action_spec().num_values)
     return np.random.choice(actions)
 
-env = dancing_catch.DancingCatch() #(swap_every=SWAP_EVERY)
+env = dancing_catch.DancingCatch(swap_every=SWAP_EVERY)
 
 replay_memory = deque(maxlen=BUFFER_SIZE)
 
-online_network = DQN(env, hidden_size=HIDDEN_LAYERS_SIZE)
-target_network = DQN(env, hidden_size=HIDDEN_LAYERS_SIZE)
+online_network = DQN(env, hidden_size=HIDDEN_LAYERS_SIZE, name='online')
+target_network = DQN(env, hidden_size=HIDDEN_LAYERS_SIZE, name='target')
 
 target_network.load_state_dict(online_network.state_dict())
 optimizer = torch.optim.Adam(online_network.parameters(), lr=LEARNING_RATE) # change to regular  beta1: 0.9, beta2 0.99
@@ -130,19 +146,23 @@ def write_list_to_file(my_list, file_name):
     except IOError:
         print("Error: Unable to write to file.")
 
-hidden_layer_weight_l2_norm = []
-hidden_layer_gradient_l0_norm = []  # List to store L0 norm values at different epochs
-hidden_layer_gradient_l1_norm = []  # List to store L1 norm values at different epochs
-hidden_layer_gradient_l2_norm = [] 
 
+hidden_layer_weight = []
+hidden_layer_gradient =[]
+activations = []
 train_losses = []
+train_losses2 = []
+l2train_losses=[]
 rewards_log = []
 catch_or_miss = [0]
 p_bar = tqdm(range(MAX_STEPS))
 gloss = 0
 total_reward = 0
+
 set_random_seed()
+
 for step in p_bar:
+   
     #Epsilon greedy with decaying epsilon between [EPSILON_START, EPSILON_END]
     epsilon = 0.1 #fix
     rnd_sample = random.random()
@@ -193,8 +213,12 @@ for step in p_bar:
 
     loss = criterion(action_q_values, targets)
     mean_loss = loss.detach().cpu().item() / BATCH_SIZE
+
+
     gloss += mean_loss
-    train_losses.append(mean_loss)
+    train_losses2.append(mean_loss)
+    if (step + 1 ) % 5000 == 0:
+        train_losses.append(mean_loss)
 
     p_bar.set_description(f"gLoss: {gloss / (step + 1) * 1000:0.5f}, reward: {reward_avg:0.5f}, mean loss: {mean_loss:.5f}")
 
@@ -206,40 +230,53 @@ for step in p_bar:
         param.grad.data.clamp_(-1, 1)
     optimizer.step()
 
+
+   
     # Update target net
     if (step + 1) % TARGET_UPDATE_FREQ == 0:
         target_network.load_state_dict(online_network.state_dict())
 
-    hidden_layer_weights = online_network.fc1.weight.data.clone().detach()  # Clone and detach to prevent gradient tracking
-    l2_norm_weight = torch.norm(hidden_layer_weights, p=2, dim=1)  # Calculate L2 norm along the rows
-    hidden_layer_weight_l2_norm.append(l2_norm_weight.mean().item())  # Store the mean L2 norm value as a scalar
+    if (step + 1 ) % 5000 == 0:
 
-    hidden_layer_gradients = online_network.fc1.weight.grad  # Get gradients of hidden layer weights
-    
-    # Calculate L0, L1, L2 norms
-    l0_norm_grad = torch.norm(hidden_layer_gradients, p=0, dim=1).mean().item()  # L0 norm
-    l1_norm_grad = torch.norm(hidden_layer_gradients, p=1, dim=1).mean().item()  # L1 norm
-    l2_norm_grad = torch.norm(hidden_layer_gradients, p=2, dim=1).mean().item()  # L2 norm
-    
-    hidden_layer_gradient_l0_norm.append(l0_norm_grad)
-    hidden_layer_gradient_l1_norm.append(l1_norm_grad)
-    hidden_layer_gradient_l2_norm.append(l2_norm_grad)
+        hidden_layer_weight.append(online_network.fc1.weight.data.detach().clone())  
 
-# import matplotlib.pyplot as plt
-# plt.figure(figsize=(8, 6))
-# plt.plot(range(MAX_STEPS), hidden_layer_weight_l2_norm, marker='o', linestyle='-', color='blue')
-# plt.title('L2 Norm of Hidden Layer Weights Over Epochs')
-# plt.xlabel('Epoch')
-# plt.ylabel('L2 Norm Value')
-# plt.grid(True)
-# plt.show()
+      # Get gradients of hidden layer weights
+        hidden_layer_gradient.append(online_network.fc1.weight.grad.detach().clone())
 
-write_list_to_file(rewards_log, "rewards_log.txt")
-write_list_to_file(train_losses, "train_losses.txt")
-write_list_to_file(hidden_layer_weight_l2_norm, "hidden_layer_weight_l2_norm.txt")
-write_list_to_file(hidden_layer_gradient_l0_norm, "hidden_layer_gradient_l0_norm.txt")
-write_list_to_file(hidden_layer_gradient_l1_norm, "hidden_layer_gradient_l1_norm.txt")
-write_list_to_file(hidden_layer_gradient_l2_norm, "hidden_layer_gradient_l2_norm.txt")
-write_list_to_file(online_network.activation_l0norm, "activation_l0_norm.txt")
-write_list_to_file(online_network.activation_l1norm, "activation_l1_norm.txt")
-write_list_to_file(online_network.activation_l2norm, "activation_l2_norm.txt")
+        
+        activations.append(online_network.plate)
+
+    # print("online_network.fc1_input: ", online_network.fc1_input.shape)
+    # print("online_network.fc1_input: ", online_network.fc1_output.shape)
+    # print("online_network.fc1_input: ",online_network.act_output.shape)
+    # print("online_network.output_output: ", online_network.output_output.shape)
+
+
+
+
+
+
+activationFunction ='Relu'
+myseed =1
+
+path = f"seed{myseed}_maxsteps{MAX_STEPS}_swap{SWAP_EVERY}_{activationFunction}_"
+
+
+with open(path + 'activations.pkl', 'wb') as f:
+        pickle.dump(activations, f)
+
+with open(path + 'hidden_layer_gradient.pkl', 'wb') as f:
+        pickle.dump(hidden_layer_gradient, f)
+
+with open(path + 'hidden_layer_weight.pkl', 'wb') as f:
+        pickle.dump(hidden_layer_weight, f)
+
+with open(path + 'loss.pkl', 'wb') as f:
+        pickle.dump(train_losses, f)
+
+with open(path + 'reward.pkl', 'wb') as f:
+        pickle.dump(rewards_log, f)
+        
+        
+print('Saved!')
+
